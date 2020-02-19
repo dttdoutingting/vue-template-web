34e93e379f8c6b358fcef3096035ec1df46ac61b
wendy <dttdoutingting@163.com>
2020-02-19 22:19:14 +0800

    配置babel-polyfill
    Signed-off-by: wendy <dttdoutingting@163.com>

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
 
 import 'common/stylus/index.styl'
 
@@ -11,7 +10,7 @@ import 'common/stylus/index.styl'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  // router,
   components: { App },
   template: '<App/>'
 })
