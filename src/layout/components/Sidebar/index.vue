cfbe7ad7fdbd3695c95d2b3cce74a4b3c503299b
wendy <dttdoutingting@163.com>
2020-02-20 11:27:22 +0800

    创建导航栏、Header等布局文件

---
+++ src/layout/components/Sidebar/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="{'has-logo':showLogo}">
+    <logo v-if="showLogo" :collapse="isCollapse" />
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+        :default-active="activeMenu"
+        :collapse="isCollapse"
+        :background-color="variables.menuBg"
+        :text-color="variables.menuText"
+        :unique-opened="false"
+        :active-text-color="variables.menuActiveText"
+        :collapse-transition="false"
+        mode="vertical"
+      >
+        <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path" />
+      </el-menu>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+import Logo from './Logo'
+import SidebarItem from './SidebarItem'
+export default {
+  components: { SidebarItem, Logo }
+}
+</script>
